75ed46834520a757bb7e3e212e222c047271a108
Gileade Trindade <gileade.trindade@gmail.com>
2020-04-13 13:25:45 -0300

    Add método getGifs

---
+++ lib/ui/home_page.dart
@@ -0,0 +1,32 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  String _search;
+  int _offset = 0;
+
+  Future<Map> _getGifs() async {
+    http.Response response;
+
+    if (_search != null)
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/trending?api_key=aa7qvWZSsrxa0DvkrxWgyhOQ9shVSX5q&limit=20&rating=G");
+    else
+      response = await http.get(
+          "https://api.giphy.com/v1/gifs/search?api_key=aa7qvWZSsrxa0DvkrxWgyhOQ9shVSX5q&q=$_search&limit=20&offset=$_offset&rating=G&lang=en");
+
+    return json.decode(response.body);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
